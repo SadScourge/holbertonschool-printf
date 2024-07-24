5c5e59a2eb4358ba6c9a0c0c53990311414cb1a4
SadScourge <darklzar@gmail.com>
2024-07-24 14:47:19 +0200

    Init of the get_format function (this one will check if the format specifier is found in our list)

---
+++ get_format.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_format - check if the given format is found in the list
+ * @format: the character corresponding to the format
+ * Return: pointer to the matching function, NULL if non is found
+ */
+int (*get_format(const char *format))(va_list)
+{
+	int index;
+	form_l array[] = {
+		{"c", print_character},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}
+	};
+
+	for (index = 0; array[index].form_specifier; index++)
+	{
+		if (*format == *(array[index].form_specifier))
+		{
+			return (array[index].function);
+		}
+	}
+	return (NULL);
+}
